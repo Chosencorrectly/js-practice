bac6a16d5b6ff5a447bd428c7df1ceb22d461d8a
Mikhail <praytomysunrise@gmail.com>
2020-05-12 23:43:49 +0300

    Usloviya

--- Version 1/script.js
+++ Version 1/script.js
@@ -1,1 +1,27 @@
-console.log('arr' + ' - object');
+let num = 50;
+
+if (num < 49) {
+    console.log("Неверно!")
+} else if (num > 100) {
+    console.log("Многовато!")
+} else {
+    console.log("Верно!")
+}
+
+switch (num) {
+    case num < 49:
+        console.log("Неверно!");
+        break;
+    case num > 100:
+        console.log("Многовато");
+        break;
+    case num > 80:
+        console.log("Все еще многовато!");
+        break;
+    case 50:
+        console.log("Верно!");
+        break;
+    default:
+        console.log("Что-то пошло не так");
+        break;
+}
