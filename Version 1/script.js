0760f14fe481adb7a0ac86c29f838ee86ffad8ef
Mikhail <praytomysunrise@gmail.com>
2020-05-13 16:59:52 +0300

    Lesson

--- Version 1/script.js
+++ Version 1/script.js
@@ -1,27 +1,36 @@
-let num = 50;
+let money = +prompt('Ваш бюджет на месяц?', ['В рублях']);
+let time = prompt('Введите дату в формате YYYY-DD-MM', ['2020-13-05']);
+let mandatoryExpense = prompt('Введите обязательную статью расходов на этот месяц');
 
-if (num < 49) {
-    console.log("Неверно!")
-} else if (num > 100) {
-    console.log("Многовато!")
+if (mandatoryExpense != false) {
+    var mandatoryExpenseCost = + prompt("Сколько это будет стоить?");
 } else {
-    console.log("Верно!")
-}
+    alert("Вы не указали статью расходов");
+};
 
-switch (num) {
-    case num < 49:
-        console.log("Неверно!");
-        break;
-    case num > 100:
-        console.log("Многовато");
-        break;
-    case num > 80:
-        console.log("Все еще многовато!");
-        break;
-    case 50:
-        console.log("Верно!");
-        break;
-    default:
-        console.log("Что-то пошло не так");
-        break;
-}
+let expenses = new Object();
+let optionalExpenses = new Array();
+let income = new Array();
+let savings = false;
+
+let appData = {
+    moneyData: money,
+    timeData: time,
+    expenses,
+    optionalExpenses,
+    income,
+    savings,
+};
+
+let dailyBudget = (money + mandatoryExpenseCost) / 30;
+
+alert("Ваш дневной бюджет: " + dailyBudget);
+
+console.log(mandatoryExpenseCost);
+console.log(typeof(mandatoryExpenseCost));
+console.log(appData.moneyData);
+console.log(appData.timeData);
+console.log(mandatoryExpense == true);
+console.log(typeof(expenses));
+console.log(typeof(optionalExpenses));
+console.log(appData);
